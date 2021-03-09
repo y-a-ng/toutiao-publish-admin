6567e9fbda4bfb18ebee2839fa1f0ff87e9d39ba
不留 <384378252@qq.com>
2021-03-09 08:38:25 +0800

    项目初始化-调整初始b目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+/* 项目的启动入口 */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+// 加载全局样式文件
+import './styles/index.less'
 
+Vue.config.productionTip = false
+// 创建Vue根实例
+// 把 router配置到根实例中
+// 通过 render 方法把 App 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: "#app" 等价于 $mount("#app")
 }).$mount('#app')
